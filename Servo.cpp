58520e315f8e672e4a571d600a5707a180e24761
artem-kondratew <artemkondratev5@gmail.com>
2023-02-15 17:17:49 +0300

    libs.cpp added

--- Servo.cpp
+++ Servo.cpp
@@ -1,31 +1,30 @@
 //
-// Created by user on 15.02.23.
+// Created by artem-kondratew on 15.02.23.
 //
 
 #include "Servo.h"
 
 
-Servo::Servo(uint16_t _min_angle, uint16_t _max_angle) {
+Servo::Servo(uint8_t dxl_id, uint16_t _min_angle, uint16_t _max_angle) {
+    DXL_ID = dxl_id;
     min_angle = _min_angle;
     max_angle = _max_angle;
     angle = min_angle;
-}
-
-Servo::Servo(uint16_t _min_angle, uint16_t _max_angle, uint16_t _angle): Servo(_min_angle, _max_angle) {
-    angle = _angle;
+    //SERVOS.ping(DXL_ID, nullptr, nullptr);
 }
 
 
 void Servo::setAngle(uint16_t _angle) {
     if (_angle < min_angle) {
         angle = min_angle;
-        return;
     }
     if (_angle > max_angle) {
         angle = max_angle;
-        return;
     }
-    angle = _angle;
+    if (min_angle <= angle <= max_angle) {
+        angle = _angle;
+    }
+    //SERVOS.goalPosition(DXL_ID, angle);
 }
 
 uint16_t Servo::getAngle() const {
--- Servo.h
+++ Servo.h
@@ -1,5 +1,5 @@
 //
-// Created by user on 15.02.23.
+// Created by artem-kondratew on 15.02.23.
 //
 
 #ifndef MANIPULATOR_SERVO_H
@@ -11,16 +11,17 @@
 
 class Servo {
 private:
+
     uint16_t min_angle;
     uint16_t max_angle;
     uint16_t angle;
+    uint8_t DXL_ID;
 public:
-    Servo(uint16_t _min_angle, uint16_t _max_angle);
-    Servo(uint16_t _min_angle, uint16_t _max_angle, uint16_t _angle);
+    //static DynamixelWorkbench SERVOS;
+    Servo(uint8_t dxl_id, uint16_t _min_angle, uint16_t _max_angle);
     ~Servo() = default;
     void setAngle(uint16_t _angle);
     [[nodiscard]] uint16_t getAngle() const;
-
 };
 
 
