c3a65be15f9004db381b3f3900a1fa4b2f22bea1
artem-kondratew <artemkondratev5@gmail.com>
2023-02-28 15:00:45 +0300

    OOP used

--- Manipulator.ino
+++ Manipulator.ino
@@ -1,16 +1,12 @@
 
 #include <DynamixelWorkbench.h>
+#include "Config.h"
+#include "Debug.h"
+#include "Joystick.h"
 #include "Servo.h"
 
 
-
-
-
 void setup() {
-    Servo servo1(DXL_ID1, 150, 873);
-    Servo servo2(DXL_ID2, 0, 1023);
-    Servo servo3(DXL_ID3, 0, 1023);
-    Servo servo4(DXL_ID4, 0, 1023);
 
     pinMode(ANALOG_PIN1, INPUT);
     pinMode(ANALOG_PIN2, INPUT);
@@ -18,38 +14,34 @@ void setup() {
     pinMode(ANALOG_PIN4, INPUT);
 
     Serial.begin(SERIAL_BAUDRATE);
-    
-    servos.init(DEVICE_NAME, SERVO_BAUDRATE);
 
+    servos.init(DEVICE_NAME, BAUDRATE);
 
-//Включение сервоприводов для движения: (ID, скорость, ускорение)
+    servo1.pingServo();
+    servo2.pingServo();
+    servo3.pingServo();
+    servo4.pingServo();
+
     servos.jointMode(DXL_ID1, 40, 40);
     servos.jointMode(DXL_ID2, 40, 40);
     servos.jointMode(DXL_ID3, 40, 40);
     servos.jointMode(DXL_ID4, 40, 40);
 
-//Ставим манипулятор в "центральное" положение
-    servos.goalPosition(DXL_ID1, 500);
-    servos.goalPosition(DXL_ID2, 500);
-    servos.goalPosition(DXL_ID3, 500);
-    servos.goalPosition(DXL_ID4, 500);
-    delay (2000);
+    getStartPosition(servos);
 }
 
 
 void loop() {
-    Serial.print("Потенциометр 1: ");
-    Serial.println(analogRead(ANALOG_PIN1));
-    Serial.print("Потенциометр 2: ");
-    Serial.println(analogRead(ANALOG_PIN2));
-    Serial.print("Потенциометр 3: ");
-    Serial.println(analogRead(ANALOG_PIN3));
-    Serial.print("Потенциометр 4: ");
-    Serial.println(analogRead(ANALOG_PIN4));
-    Serial.print("\n");
+  
+    debugPrint();
 
-    //servo1.setAngle(map((int)analogRead(ANALOG_PIN1), 0, 1023, 150, 873));
-    servos.goalPosition(DXL_ID2, map((int)analogRead(ANALOG_PIN2), 0, 1023, 400, 660));
-    servos.goalPosition(DXL_ID3, map((int)analogRead(ANALOG_PIN3), 0, 1023, 200, 600));
-    servos.goalPosition(DXL_ID4, map((int)analogRead(ANALOG_PIN4), 0, 1023, 500, 1000));
+    Joystick::refresh();
+
+    servo1.setAngle(joystick1.getValue());
+    servo2.setAngle(joystick2.getValue());
+    servo3.setAngle(joystick3.getValue());
+    servo4.setAngle(joystick4.getValue());
+
+    Serial.print("checker: ");
+    Serial.println(Joystick::checkAngles());
 }
--- Servo.h
+++ Servo.h
@@ -1,6 +1,6 @@
+
 #include <stdint.h>
 #include <DynamixelWorkbench.h>
-#include "Config.h"
 
 
 DynamixelWorkbench servos;
@@ -15,14 +15,26 @@ private:
 public:
     Servo(uint8_t _DXL_ID, uint16_t _min_angle, uint16_t _max_angle);
     ~Servo() = default;
+    void pingServo();
     void setAngle(uint16_t _angle);
+    uint16_t getAngle();
 };
+
+
+Servo servo1(DXL_ID1, 150, 853);
+Servo servo2(DXL_ID2, 0, 590);
+Servo servo3(DXL_ID3, 270, 1023);
+Servo servo4(DXL_ID4, 0, 1023);
 
 
 Servo::Servo(uint8_t _DXL_ID, uint16_t _min_angle, uint16_t _max_angle) {
     DXL_ID = _DXL_ID;
     min_angle = _min_angle;
     max_angle = _max_angle;
+}
+
+
+void Servo::pingServo() {
     servos.ping(DXL_ID, 0, 0);
 }
 
@@ -30,15 +42,20 @@ Servo::Servo(uint8_t _DXL_ID, uint16_t _min_angle, uint16_t _max_angle) {
 void Servo::setAngle(uint16_t _angle) {
     if (_angle < min_angle) {
         angle = min_angle;
+        servos.goalPosition(DXL_ID, angle);
+        return;
     }
     if (_angle > max_angle) {
-        angle = max_angle;
+      angle = max_angle;
+        servos.goalPosition(DXL_ID, angle);
+        return;
     }
-    if (min_angle <= _angle <= max_angle) {
-        angle = _angle;
-    }
+    angle = _angle;
     servos.goalPosition(DXL_ID, angle);
+    return;
 }
 
 
-//test_comment
+uint16_t Servo::getAngle() {
+    return angle;
+}
