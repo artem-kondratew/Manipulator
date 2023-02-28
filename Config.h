d360ebff58500f5e787d7391b211de4ddd2d0461
artem-kondratew <artemkondratev5@gmail.com>
2023-02-28 14:59:56 +0300

    getStartPosition moved

--- Config.h
+++ Config.h
@@ -1,7 +1,7 @@
 
 #define DEVICE_NAME "3"
 
-#define SERVO_BAUDRATE 1000000
+#define BAUDRATE 1000000
 #define SERIAL_BAUDRATE 57600
 
 #define DXL_ID1 1
@@ -13,3 +13,14 @@
 #define ANALOG_PIN2 A1
 #define ANALOG_PIN3 A2
 #define ANALOG_PIN4 A3
+
+#define BUTTON_PIN 9
+
+
+void getStartPosition(DynamixelWorkbench servos) {
+    servos.goalPosition(DXL_ID1, 500);
+    servos.goalPosition(DXL_ID2, 500);
+    servos.goalPosition(DXL_ID3, 500);
+    servos.goalPosition(DXL_ID4, 500);
+    delay (5000);
+}
