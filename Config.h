2d8ced7d38a101bea3b5c5e0ca568955beb8cf49
artem-kondratew <artemkondratev5@gmail.com>
2023-03-17 23:35:40 +0300

    analog pins deleted

--- Config.h
+++ Config.h
@@ -16,13 +16,6 @@
 #define DXL_ID3 3
 #define DXL_ID4 4
 
-#define ANALOG_PIN1 A0
-#define ANALOG_PIN2 A1
-#define ANALOG_PIN3 A2
-#define ANALOG_PIN4 A3
-
-#define BUTTON_PIN 9
-
 
 #define START_BYTE 64
 #define CONNECT_DXL_ID 0
