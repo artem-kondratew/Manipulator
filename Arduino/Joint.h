529ddfef5b754fcd2d048a6d2dc7bfea0b613d34
reiv27 <mr.eidelman@gmail.com>
2023-03-28 17:48:03 +0300

    isn't important 3

--- Arduino/Joint.h
+++ Arduino/Joint.h
@@ -6,6 +6,14 @@
 
 #define PI 3.14
 
+// Lengths of links in mm:
+#define X0 -25
+#define Z0 100
+#define LEN1 103
+#define LEN2 150
+#define LEN3 150
+#define LEN4 120
+
 
 class Joint {
 private:
@@ -13,35 +21,157 @@ private:
     // Coordinates in mm.
     int32_t x;
     int32_t y;
-    int32_t z;    
+    int32_t z;
+    int8_t q0;
+    int8_t q1;
+    int8_t q2;
+    double q0_rad;
+    double q1_rad;
+    double q2_rad;
 public:
     Joint(uint8_t _DXL_ID);
     ~Joint() = default;
     void GoTo(int32_t my_x, int32_t my_y, int32_t my_z);
+    void getRadians();
+    int32_t getX();
+    int32_t getY();
+    int32_t getZ();
     void getCoordinates();
-    static void getAngles();
     
 };
 
 
 Joint::Joint(uint8_t _DXL_ID) {
     DXL_ID = _DXL_ID;
+}
 
-    // Lengths of links in mm:
-    const uint8_t x0 = -25;
-    const uint8_t z0 = 100;
-    const uint8_t len1 = 103;
-    const uint8_t len2 = 150;
-    const uint8_t len3 = 150;
-    const uint8_t len4 = 120;
+
+void Joint::getRadians() {
+    q0 = map(servo1.getAngle(), 0, 1023, -179, 180);
+    q1 = map(servo2.getAngle(), 0, 1023, -240, -88);
+    q2 = map(servo3.getAngle(), 0, 1023, -122, 237);
+    q0_rad = q0 / 180.0 * PI;
+    q1_rad = q1 / 180.0 * PI;
+    q2_rad = q2 / 180.0 * PI;
+}
+
+
+int32_t Joint::getX() {
+    getRadians();
+    switch (DXL_ID) {
+        case 1: {
+            x = X0 * cos(q0_rad);
+            break;
+        }
+        case 2: {
+            x = (X0 + LEN2 * cos(q1_rad)) * cos(q0_rad);
+            break;  
+        }
+        case 3: {
+            x = (X0 + LEN2 * cos(q1_rad) + LEN3 * cos(q2_rad)) * cos(q0_rad);
+            break;  
+        }
+        case 4: {
+            x = (X0 + LEN2 * cos(q1_rad) + LEN3 * cos(q2_rad) + LEN4) * cos(q0_rad);
+            break;
+        }
+        default:
+            Serial.print("Wrong DXL_ID!");
+            break;
+    }
+    
+    return x;
+}
+
+
+int32_t Joint::getY() {
+    getRadians();
+    switch (DXL_ID) {
+        case 1: {
+            y = X0 * sin(q0_rad);
+            break;
+        }
+        case 2: {
+            y = (X0 + LEN2 * cos(q1_rad)) * sin(q0_rad);
+            break;  
+        }
+        case 3: {
+            y = (X0 + LEN2 * cos(q1_rad) + LEN3 * cos(q2_rad)) * sin(q0_rad);
+            break;  
+        }
+        case 4: {
+            y = (X0 + LEN2 * cos(q1_rad) + LEN3 * cos(q2_rad) + LEN4) * sin(q0_rad);
+            break;
+        }
+    }
+}
+
+
+int32_t Joint::getZ() {
+    getRadians();
+    switch (DXL_ID) {
+        case DXL_ID1: {
+            z = Z0;
+            break;
+        }
+        case 2: {
+            z = Z0 + LEN2 * sin(q1_rad);
+            break;  
+        }
+        case 3: {
+            z = Z0 + LEN2 * sin(q1_rad) + LEN3 * sin(q2_rad);
+            break;  
+        }
+        case 4: {
+            z = Z0 + LEN2 * sin(q1_rad) + LEN3 * sin(q2_rad);
+            break;
+        }
+        default:
+            Serial.print("Wrong DXL_ID!");
+            break;
+    }
+    
+    return z;
+}
+
+
+void Joint::getCoordinates() {
+    Serial.print("Angle q0: ");
+    Serial.print(q0); Serial.print(" "); Serial.println(q0_rad);
+    Serial.print("Angle q1: ");
+    Serial.print(q1); Serial.print(" "); Serial.println(q1_rad); 
+    Serial.print("Angle q2: ");
+    Serial.print(q2); Serial.print(" "); Serial.println(q2_rad);
+    
+    Serial.print("Coordinate x: ");
+    Serial.println(getX());
+    Serial.print("Coordinate y: ");
+    Serial.println(getY());  
+    Serial.print("Coordinate z: ");
+    Serial.println(getZ());
+    Serial.println();
+}
+
+
+/* void Joint::getAngles() {
     int8_t q0 = map(servo1.getAngle(), 0, 1023, -179, 180);
     int8_t q1 = map(servo2.getAngle(), 0, 1023, -240, -88);
-    int8_t q2 = map(servo3.getAngle(), 0, 1023, -233, 126);
-    double q0_rad = q0 / 180 * PI;
-    double q1_rad = q1 / 180 * PI;
-    double q2_rad = q2 / 180 * PI;
+    int8_t q2 = map(servo3.getAngle(), 0, 1023, 388, 0);
+    double q0_rad = q0 / 180.0 * PI;
+    double q1_rad = q1 / 180.0 * PI;
+    double q2_rad = q2 / 180.0 * PI;
     
-    switch (DXL_ID) {
+
+} */
+
+
+Joint joint1(DXL_ID1);
+Joint joint2(DXL_ID2);
+Joint joint3(DXL_ID3);
+Joint joint4(DXL_ID4);
+
+
+/*    switch (DXL_ID) {
         case 1: {
             x = x0 * cos(q0_rad);
             y = x0 * sin(q0_rad);
@@ -61,50 +191,12 @@ Joint::Joint(uint8_t _DXL_ID) {
             break;  
         }
         case 4: {
-            x = (x0 + len2 * cos(1.5708) + len3 * cos(0) + len4) * cos(0);
-            y = (x0 + len2 * cos(1.5708) + len3 * cos(0) + len4) * sin(0);
-            z = z0 + len2 * sin(1.5708) + len3 * sin(0);
-            break;  
+            x = (x0 + len2 * cos(q1_rad) + len3 * cos(q2_rad) + len4) * cos(q0_rad);
+            y = (x0 + len2 * cos(q1_rad) + len3 * cos(q2_rad) + len4) * sin(q0_rad);
+            z = z0 + len2 * sin(q1_rad) + len3 * sin(q2_rad);
+            break;
         }
         default:
             Serial.print("Wrong DXL_ID!");
             break;
-    }
-}
-
-
-void GoTo(int32_t x, int32_t y, int32_t z);
-
-
-void Joint::getCoordinates() {
-    Serial.print("Coordinate x: ");
-    Serial.println(x);
-    Serial.print("Coordinate y: ");
-    Serial.println(y);  
-    Serial.print("Coordinate z: ");
-    Serial.println(z);
-    Serial.println();
-}
-
-
-void Joint::getAngles() {
-    int8_t q0 = map(servo1.getAngle(), 0, 1023, -179, 180);
-    int8_t q1 = map(servo2.getAngle(), 0, 1023, -240, -88);
-    int8_t q2 = map(servo3.getAngle(), 0, 1023, 388, 0);
-    double q0_rad = q0 / 180.0 * PI;
-    double q1_rad = q1 / 180.0 * PI;
-    double q2_rad = q2 / 180.0 * PI;
-    
-    Serial.print("Angle q0: ");
-    Serial.print(q0); Serial.print(" "); Serial.println(q0_rad);
-    Serial.print("Angle q1: ");
-    Serial.print(q1); Serial.print(" "); Serial.println(q1_rad); 
-    Serial.print("Angle q2: ");
-    Serial.print(q2); Serial.print(" "); Serial.println(q2_rad);
-}
-
-
-Joint joint1(1);
-Joint joint2(2);
-Joint joint3(3);
-Joint joint4(4);
+    } */
